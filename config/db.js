54e1d948ebc79c66c44d657691d684323361840e
Kelenna <kellykowe12@gmail.com>
2023-07-05 14:01:18 +0100

    Even more changes

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDb = async () => {
   try {
-    const conn = await mongoose.connect("mongodb+srv://Kelenna:Niklauz2497@cluster0.rdbnat3.mongodb.net/", {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       //    useCreateIndex: true
